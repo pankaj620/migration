294b6bdc884bc279e3af27a84f15e297724ed871
pankaj620 <59977514+pankaj620@users.noreply.github.com>
2022-01-25 02:22:42 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,9 +1,6 @@
 provider "azurerm"{
   version="<=2.0.0"
   subscription_id=var.subscriptionId
-  cleint_id=var.clientId
-  client_secret=var.clientsecret
-  tenant_id=var.tenantId
   features{}
 }
 resource "azurerm_resource_group" "example" {
