6b1f7f4512c5c48d38e82daaa0461cf67d8a5312
pankaj620 <59977514+pankaj620@users.noreply.github.com>
2022-01-25 00:35:04 +0530

    Update vars.tf

--- vars.tf
+++ vars.tf
@@ -1,11 +1,18 @@
 variable "location"{
+ type=string
 }
 variable "subscriptionId"{
+ type=string
 }
 variable "RGname"{
+ type=string
 }
 variable "tenantId"{
+ type=string
 }
-variable "secret"{
+variable "clientsecret"{
+ type=string
+}
+variable "clientId"{
 }
 
